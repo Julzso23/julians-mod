f9e70e9abb5bb2eda9902b61b32ae518a4eaa272
Julian Bath <julzso23@gmail.com>
2019-02-28 19:02:33 +0000

    Added common and client proxy class paths to reference class

--- src/main/java/website/julians/juliansmod/util/Reference.java
+++ src/main/java/website/julians/juliansmod/util/Reference.java
@@ -6,6 +6,6 @@ public class Reference
     public static final String NAME = "Julian's Mod";
     public static final String VERSION = "1.0.0";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
-    public static final String CLIENT_PROXY_CLASS = "";
-    public static final String COMMON_PROXY_CLASS = "";
+    public static final String CLIENT_PROXY_CLASS = "website.julians.juliansmod.proxy.ClientProxy";
+    public static final String COMMON_PROXY_CLASS = "website.julians.juliansmod.proxy.CommonProxy";
 }
