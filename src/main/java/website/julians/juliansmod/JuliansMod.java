5b6f73717a4b90d23119cf481f7db0942464058e
Julian Bath <julzso23@gmail.com>
2019-02-28 19:16:46 +0000

    Added initialisation events

--- src/main/java/website/julians/juliansmod/JuliansMod.java
+++ src/main/java/website/julians/juliansmod/JuliansMod.java
@@ -1,6 +1,11 @@
 package website.julians.juliansmod;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import website.julians.juliansmod.proxy.CommonProxy;
 import website.julians.juliansmod.util.Reference;
 
 @Mod(
@@ -11,4 +16,24 @@ import website.julians.juliansmod.util.Reference;
 )
 public class JuliansMod
 {
+    @Mod.Instance
+    public static JuliansMod instance;
+
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.COMMON_PROXY_CLASS)
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    public static void PreInit(FMLPreInitializationEvent event)
+    {
+    }
+
+    @Mod.EventHandler
+    public static void Init(FMLInitializationEvent event)
+    {
+    }
+
+    @Mod.EventHandler
+    public static void PostInit(FMLPostInitializationEvent event)
+    {
+    }
 }
