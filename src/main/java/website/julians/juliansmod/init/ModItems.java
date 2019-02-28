c4bcfd5b987a7540fafb9dbadb417fdeca4c7c8b
Julian Bath <julzso23@gmail.com>
2019-02-28 19:46:36 +0000

    Added mod items class

---
+++ src/main/java/website/julians/juliansmod/init/ModItems.java
@@ -0,0 +1,13 @@
+package website.julians.juliansmod.init;
+
+import net.minecraft.item.Item;
+import website.julians.juliansmod.items.ItemBase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModItems
+{
+    public static final List<Item> ITEMS = new ArrayList<Item>();
+    public static final Item TEST = new ItemBase("test");
+}
