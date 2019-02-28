8717926f26032331d7ef9bfa57e852e061dc1f8f
Julian Bath <julzso23@gmail.com>
2019-02-28 18:56:19 +0000

    Added main class

---
+++ src/main/java/website/julians/juliansmod/JuliansMod.java
@@ -0,0 +1,14 @@
+package website.julians.juliansmod;
+
+import net.minecraftforge.fml.common.Mod;
+import website.julians.juliansmod.util.Reference;
+
+@Mod(
+        modid = Reference.MOD_ID,
+        name = Reference.NAME,
+        version = Reference.VERSION,
+        acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS
+)
+public class JuliansMod
+{
+}
