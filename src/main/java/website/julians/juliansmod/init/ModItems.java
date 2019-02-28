b17fd6abcb593d40c0a27cc06bfc778c74c763a3
Julian Bath <julzso23@gmail.com>
2019-02-28 21:06:01 +0000

    Renamed test to flour

--- src/main/java/website/julians/juliansmod/init/ModItems.java
+++ src/main/java/website/julians/juliansmod/init/ModItems.java
@@ -9,5 +9,5 @@ import java.util.List;
 public class ModItems
 {
     public static final List<Item> ITEMS = new ArrayList<Item>();
-    public static final Item TEST = new ItemBase("test");
+    public static final Item FLOUR = new ItemBase("flour");
 }
