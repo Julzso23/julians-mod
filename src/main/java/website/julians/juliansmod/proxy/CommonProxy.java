fde35b37b64065c256de463079285c311436da18
Julian Bath <julzso23@gmail.com>
2019-02-28 19:46:22 +0000

    Added register item renderer function to proxies

--- src/main/java/website/julians/juliansmod/proxy/CommonProxy.java
+++ src/main/java/website/julians/juliansmod/proxy/CommonProxy.java
@@ -1,5 +1,10 @@
 package website.julians.juliansmod.proxy;
 
+import net.minecraft.item.Item;
+
 public class CommonProxy
 {
+    public void registerItemRenderer(Item item, int meta, String id)
+    {
+    }
 }
