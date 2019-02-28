494d22fc8fb1506945872b988beda6c9cb569ad8
Julian Bath <julzso23@gmail.com>
2019-02-28 21:10:39 +0000

    Updated registry handler for blocks

--- src/main/java/website/julians/juliansmod/util/handlers/RegistryHandler.java
+++ src/main/java/website/julians/juliansmod/util/handlers/RegistryHandler.java
@@ -1,10 +1,12 @@
 package website.julians.juliansmod.util.handlers;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import website.julians.juliansmod.init.ModBlocks;
 import website.julians.juliansmod.init.ModItems;
 import website.julians.juliansmod.util.IHasModel;
 
@@ -18,6 +20,12 @@ public class RegistryHandler
     }
 
     @SubscribeEvent
+    public static void onblockRegister(RegistryEvent.Register<Block> event)
+    {
+        event.getRegistry().registerAll(ModBlocks.BLOCKS.toArray(new Block[0]));
+    }
+
+    @SubscribeEvent
     public static void onModelRegister(ModelRegistryEvent event)
     {
         for (Item item : ModItems.ITEMS)
@@ -25,6 +33,14 @@ public class RegistryHandler
             if (item instanceof IHasModel)
             {
                 ((IHasModel)item).registerModels();
+            }
+        }
+
+        for (Block block : ModBlocks.BLOCKS)
+        {
+            if (block instanceof IHasModel)
+            {
+                ((IHasModel)block).registerModels();
             }
         }
     }
